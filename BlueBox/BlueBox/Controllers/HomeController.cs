3290b758935fdff0bca5d15008277bb26c2757ca
tapiad <dtapia12@mail.wou.edu>
2018-09-24 17:10:47 -0700

    Custom made the 'About' Page

--- BlueBox/BlueBox/Controllers/HomeController.cs
+++ BlueBox/BlueBox/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace BlueBox.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 

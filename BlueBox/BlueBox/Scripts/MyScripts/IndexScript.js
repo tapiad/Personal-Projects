387b9eb7cd4690ac3f5836f1e8d718f9f98a5ffa
tapiad <dtapia12@mail.wou.edu>
2018-09-26 10:58:27 -0700

    Added: comments, modified code, connected api key to application

---
+++ BlueBox/BlueBox/Scripts/MyScripts/IndexScript.js
@@ -0,0 +1,11 @@
+﻿onload = LoadNewMovies()
+
+function LoadNewMovies() {
+    console.log("You're in the IndexScript.js Script");
+
+    // Create Source for GET JSON Request
+    var source = "https://api.themoviedb.org/3/movie/now_playing?region=US&api_key=" + movieAPIKey;
+
+    console.log("Source: " + source);
+
+}

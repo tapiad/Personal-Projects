f82b169de6dac53bdbe18238fef63b0701bba5d0
tapiad <dtapia12@mail.wou.edu>
2018-09-26 14:50:26 -0700

    Connected MovieDB API to the application for 'Now Playing' Movies

--- BlueBox/BlueBox/Scripts/MyScripts/IndexScript.js
+++ BlueBox/BlueBox/Scripts/MyScripts/IndexScript.js
@@ -8,4 +8,57 @@ function LoadNewMovies() {
 
     console.log("Source: " + source);
 
+    // Get 'Now Playing' Movies
+    // Get JSON object from AJAX call
+    $.ajax({
+        url: source,
+        method: "GET",
+        success: getNowPlayingMovies,
+        error: errorOnAjax
+    });
+}
+
+
+// Display 'Now Playing'  Movie Results
+function getNowPlayingMovies(data) {
+    console.log("Total Results: " + data.total_results);
+
+    // URL to Movie's Poster
+    var posterURL = 'https://image.tmdb.org/t/p/w400';
+
+    //Display First 5 Results
+    for (i = 0; i < 5; i++) {
+        var result = data.results[i];
+
+        // Clean 
+        var carSlider = "";
+        // Movie's Poster Image
+        var posterImg = posterURL + result.poster_path;
+        // Movie's Title
+        var movieTitle = result.original_title;
+
+        // Set First Carousel Active
+        if (i == 0) {
+            carSlider = carSlider.concat('<div class="carousel-item active">');
+        } else {
+            carSlider = carSlider.concat('<div class="carousel-item">');
+        }
+
+        // Create Movie Poster
+        carSlider = carSlider.concat(
+            '<img class="img-fluid" src="' + posterImg + '" alt=' + movieTitle + '>' +                
+            '</div>');
+
+        // Add Movie Poster to Carousel Slider
+        $("#carouselSlider").append(carSlider);
+    }
+    
+
+}
+
+
+// Ajax Error 
+function errorOnAjax(e) {
+    console.log("Error on Ajax: " + e.error);
+    return false;
 }

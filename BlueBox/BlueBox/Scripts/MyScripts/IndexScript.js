9100995cf5705415bef7f4dc3de7621e68520077
tapiad <dtapia12@mail.wou.edu>
2018-10-10 10:57:25 -0700

    Added Title, Overview; avg vote for each 'now playing' movies onto home page

--- BlueBox/BlueBox/Scripts/MyScripts/IndexScript.js
+++ BlueBox/BlueBox/Scripts/MyScripts/IndexScript.js
@@ -19,6 +19,7 @@ function LoadNewMovies() {
 }
 
 
+
 // Display 'Now Playing'  Movie Results
 function getNowPlayingMovies(data) {
     console.log("Total Results: " + data.total_results);
@@ -26,16 +27,24 @@ function getNowPlayingMovies(data) {
     // URL to Movie's Poster
     var posterURL = 'https://image.tmdb.org/t/p/w400';
 
+
     //Display First 5 Results
     for (i = 0; i < 5; i++) {
         var result = data.results[i];
 
-        // Clean 
+        // Clear variable
         var carSlider = "";
-        // Movie's Poster Image
+        // Get Movie's Poster Image
         var posterImg = posterURL + result.poster_path;
-        // Movie's Title
+        // Get Movie's Title
         var movieTitle = result.original_title;
+        // Get Vote Count
+        var voteCount = result.vote_count;
+        // Get Vote Average
+        var voteAvg = result.vote_average;
+        // Get Moview Overview
+        var movieOverview = result.overview;
+
 
         // Set First Carousel Active
         if (i == 0) {
@@ -44,9 +53,17 @@ function getNowPlayingMovies(data) {
             carSlider = carSlider.concat('<div class="carousel-item">');
         }
 
+
         // Create Movie Poster
         carSlider = carSlider.concat(
-            '<img class="img-fluid" src="' + posterImg + '" alt=' + movieTitle + '>' +                
+            '<img class="img-fluid" src="' + posterImg + '" alt=' + movieTitle + '>' +
+            '<div class="carousel-caption fixed-top" id="movieInfo">' +
+                '<h1>' + movieTitle + '</h1>' +
+                '<hr />' +
+                '<h4 class="text-left">Overview:</h4>' + 
+                '<p class="text-left">' + movieOverview + '</p>' +
+            '<p class="text-left">Average Vote: ' + voteAvg + '/10 <small> -- Vote Count: ' + voteCount + '</small></p>' +  
+            '</div>' +
             '</div>');
 
         // Add Movie Poster to Carousel Slider
@@ -55,6 +72,7 @@ function getNowPlayingMovies(data) {
     
 
 }
+
 
 
 // Ajax Error 

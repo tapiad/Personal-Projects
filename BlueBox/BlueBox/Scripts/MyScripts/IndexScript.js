77094cd2e06cf99271eab517006150795dd49feb
tapiad <dtapia12@mail.wou.edu>
2018-10-11 10:23:51 -0700

    Added a button to watch trailer to movie

--- BlueBox/BlueBox/Scripts/MyScripts/IndexScript.js
+++ BlueBox/BlueBox/Scripts/MyScripts/IndexScript.js
@@ -3,10 +3,10 @@
 function LoadNewMovies() {
     console.log("You're in the IndexScript.js Script");
 
-    // Create Source for GET JSON Request
+    // Create Source for GET JSON Request for Now Playing Movies
     var source = "https://api.themoviedb.org/3/movie/now_playing?region=US&api_key=" + movieAPIKey;
 
-    console.log("Source: " + source);
+    console.log("Now Playing JSON Source: " + source);
 
     // Get 'Now Playing' Movies
     // Get JSON object from AJAX call
@@ -20,11 +20,10 @@ function LoadNewMovies() {
 
 
 
-// Display 'Now Playing'  Movie Results
+// Displays 'Now Playing' Movies onto Slider
 function getNowPlayingMovies(data) {
-    console.log("Total Results: " + data.total_results);
 
-    // URL to Movie's Poster
+    // URL to all Movie's Poster
     var posterURL = 'https://image.tmdb.org/t/p/w400';
 
 
@@ -34,16 +33,18 @@ function getNowPlayingMovies(data) {
 
         // Clear variable
         var carSlider = "";
-        // Get Movie's Poster Image
+        // Get Poster Image
         var posterImg = posterURL + result.poster_path;
-        // Get Movie's Title
+        // Get Title
         var movieTitle = result.original_title;
         // Get Vote Count
         var voteCount = result.vote_count;
         // Get Vote Average
         var voteAvg = result.vote_average;
-        // Get Moview Overview
+        // Get Overview Details
         var movieOverview = result.overview;
+        // Get Movie ID
+        var movieID = result.id;
 
 
         // Set First Carousel Active
@@ -56,22 +57,78 @@ function getNowPlayingMovies(data) {
 
         // Create Movie Poster
         carSlider = carSlider.concat(
-            '<img class="img-fluid" src="' + posterImg + '" alt=' + movieTitle + '>' +
-            '<div class="carousel-caption fixed-top" id="movieInfo">' +
-                '<h1>' + movieTitle + '</h1>' +
+            '<img class="img-fluid" src="' + posterImg + '" alt=' + movieTitle + '>' + // Poster Image
+                '<div class="carousel-caption fixed-top" id="movieInfo">' +
+                '<h1>' + movieTitle + '</h1>' + // Movie Title
                 '<hr />' +
-                '<h4 class="text-left">Overview:</h4>' + 
-                '<p class="text-left">' + movieOverview + '</p>' +
-            '<p class="text-left">Average Vote: ' + voteAvg + '/10 <small> -- Vote Count: ' + voteCount + '</small></p>' +  
-            '</div>' +
+                '<h4 class="text-left">Overview:</h4>' +
+                '<p class="text-left">' + movieOverview + '</p>' + // Movie Overview Details
+                '<p class="text-left">Average Vote: ' + voteAvg + '/10 <small> -- Vote Count: ' + voteCount + '</small></p>' + // Movie Ratings
+                '<button type="button" class="btn btn-primary video-btn" id="trailerButtonID" data-toggle="modal" data-src="' + movieID +
+                '" data-target="#myModal">Watch Trailer</button >' + // Button to Watch Trailer
+                '</div>' +
             '</div>');
 
         // Add Movie Poster to Carousel Slider
         $("#carouselSlider").append(carSlider);
     }
-    
-
 }
+
+
+// If Trailer Button Clicked...
+$(document).on("click", "#trailerButtonID", playTrailer);
+
+
+// Play Movie Trailer
+function playTrailer() {
+
+    // Get Video ID
+    var videoID = $(this).data("src");
+    console.log("Video ID: " + videoID);
+
+
+    // Create Source for GET JSON Request for Movie Trailer Video
+    var source = "https://api.themoviedb.org/3/movie/" + videoID + "/videos?api_key=" + movieAPIKey + "&language=en-US";
+
+    console.log("Trailer Video JSON Source: " + source);
+
+    // Get Movie Trailer Videos
+    // Get JSON object from AJAX call
+    $.ajax({
+        url: source,
+        method: "GET",
+        success: displayTrailerVideo,
+        error: errorOnAjax
+    });
+}
+
+
+// Get Trailer Video & Autoplay on Modal
+var videoSrc
+function displayTrailerVideo(data) {
+    // Get Video Key
+    var videoKey = data.results[0].key;
+    console.log("Trailer Video Key: " + videoKey);
+
+    // Video Soucre to Trailer Video
+    videoSrc = "https://www.youtube.com/embed/" + videoKey;
+    console.log("Trailer Video Source: " + videoSrc);
+
+    // Autoplay Trailer onto Modal
+    $("#video").attr('src', videoSrc + "?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1");
+}
+
+
+//Creadit: https://codepen.io/JacobLett/pen/xqpEYE
+$(document).ready(function () {
+
+    // Stop Playing Video when Modal Closes
+    $('#myModal').on('hide.bs.modal', function (e) {
+        // Set to Video Source without Autoplay
+        $("#video").attr('src', videoSrc);
+    })
+ 
+});
 
 
 

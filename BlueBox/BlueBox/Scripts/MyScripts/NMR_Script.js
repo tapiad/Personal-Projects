ab65656598aa3cdb006f9500f5d7020a0021f954
tapiad <dtapia12@mail.wou.edu>
2018-12-03 19:43:16 -0800

    Added pagination. Limit of 10 pages

--- BlueBox/BlueBox/Scripts/MyScripts/NMR_Script.js
+++ BlueBox/BlueBox/Scripts/MyScripts/NMR_Script.js
@@ -1,18 +1,27 @@
 ﻿onload = LoadNMReleases();
 
+// Variable to hold date value
 var today;
+var page;
 
 function LoadNMReleases() {
     console.log("You're in the NMR_Script.js Script");
 
+    // Get Today's Date
     getDate();
+    page = getPage();
+    if (page === undefined) {
+        page = 1;
+    }
+    $("#currentPage").text("Page " + page + "/10");
 
     console.log("Today is: " + today);
+    console.log("page: " + page);
 
     // Create Source for GET JSON Request for New Movie Releases
     var source = "https://api.themoviedb.org/3/discover/movie?api_key=" + movieAPIKey +
-        "&language=en-US&sort_by=release_date.desc&include_adult=true&" +
-        "include_video=true&page=1&region=US&release_date.lte=" + today + "&with_original_language=en";
+        "&language=en-US&sort_by=release_date.desc&include_adult=false&" +
+        "include_video=false&page=" + page + "&region=US&release_date.lte=" + today + "&with_original_language=en";
 
     console.log("Now Playing JSON Source: " + source);
 
@@ -29,130 +38,77 @@ function LoadNMReleases() {
 function getNewMoviesReleases(data) {
 
     // URL to all Movie's Poster
-    var posterURL = 'https://image.tmdb.org/t/p/w200';
+    var posterURL = 'https://image.tmdb.org/t/p/w300';
+
+    $("#newMovRel_Results").text("");
 
     // Display First Page - 20 Results
-    for (i = 0; i < 20; i+=4) {
+    for (i = 0; i < 20; i+=2) {
 
         // Clear data
         var movieReleases = "";
 
-        // Data
-        //var result = data.results[i];
-
-        //// Get Poster Image
-        //var posterImg = posterURL + result.poster_path;
-        //// Get Title
-        //var movieTitle = result.original_title;
-        //// Get Vote Count
-        //var voteCount = result.vote_count;
-        //// Get Vote Average
-        //var voteAvg = result.vote_average;
-        //// Get Overview Details
-        //var movieOverview = result.overview;
-        //// Get Movie ID
-        //var movieID = result.id;
-
+        // Start a div row
         movieReleases = movieReleases.concat('<div class="row">');
 
-        for (x = 0; x < 4; x++) {
+        for (x = 0; x < 2; x++) {
+
+            // Set y to display first and second movie to each row
             var y = i + x;
 
+            // get movie poster, title, and overview 
             movieReleases = movieReleases.concat(
-                '<div class="col-sm-3">' +
-                    '<img class="img-fluid" src="' + posterURL + data.results[y].poster_path + '" alt=' + data.results[y].original_title + '>' + // Poster Image
-                '<hr />' +
-                '<p class="text-left">' + data.results[y].overview + '</p>' + // Movie Overview Details
+                '<div class="col-sm-6 text-center">' +
+                    '<span style="font-size: 2.5em; font-style: italic; text-transform: uppercase;">' +
+                        '<img class="img-fluid" src="' + posterURL + data.results[y].poster_path + '" alt=' + data.results[y].original_title + '>' + // Poster Image
+                    '</span>' +
+                    '<hr />' +
+                    '<p>' + data.results[y].overview + '</p>' + // Movie Overview Details
                 '</div>');
 
-            console.log('in: ' + y);
+            //console.log('in: ' + y);
         }
-        console.log('out');
+        //console.log('out');
 
+        // Close div row with a break 
         movieReleases = movieReleases.concat('</div><br />');
 
+        // Add reults to display
         $("#newMovRel_Results").append(movieReleases);
 
-        //movieReleases = movieReleases.concat(
-        //    '<div class="row">' +
-        //        '<div class="col-sm-3">' +
-        //    '<img class="img-fluid" src="' + posterURL + data.results[i].poster_path + '" alt=' + data.results[i].original_title + '>' + // Poster Image
-        //        '</div>' +
-        //        '<div class="col-sm-3">' +
-        //    //'<img class="img-fluid" src="' + posterURL + data.results[i+1].poster_path + '" alt=' + data.results[i+1].original_title  + '>' + // Poster Image
-        //        '</div>' +
-        //        '<div class="col-sm-3">' +
-        //            //'<img class="img-fluid" src="' + posterImg + '" alt=' + movieTitle + '>' + // Poster Image
-        //        '</div>' +
-        //        '<div class="col-sm-3">' +
-        //            //'<img class="img-fluid" src="' + posterImg + '" alt=' + movieTitle + '>' + // Poster Image
-        //        '</div>' +
-        //    '</div>');
+        
+        if (page === 1) {
+            $("#prevBtn").attr('disabled', 'disabled');
+        } else {
+            $("#prevBtn").removeAttr('disabled');
+        }
 
-        //$("#newMovRel_Results").append(movieReleases);
-
-        // Set Row opening
-        //if (i % 4 === 0) {
-        //    console.log("First i = " + i);
-        //    movieReleases = movieReleases.concat(
-        //        '<div class="row">' +
-        //        '<div class="col-sm-3">' +
-        //        '<img class="img-fluid" src="' + posterImg + '" alt=' + movieTitle + '>' + // Poster Image
-        //        //'<div class="carousel-caption fixed-top" id="movieInfo">' +
-        //        //    '<h1>' + movieTitle + '</h1>' + // Movie Title
-        //        //    '<hr />' +
-        //        //    '<h4 class="text-left">Overview:</h4>' +
-        //        //    '<p class="text-left">' + movieOverview + '</p>' + // Movie Overview Details
-        //        //    '<p class="text-left">Average Vote: ' + voteAvg + '/10 <small> -- Vote Count: ' + voteCount + '</small></p>' + // Movie Ratings
-        //        //    '<button type="button" class="btn btn-primary video-btn" id="trailerButtonID" data-toggle="modal" data-src="' + movieID +
-        //        //    '" data-target="#myModal">Watch Trailer</button >' + // Button to Watch Trailer
-        //        //'</div>' +
-        //        '</div>');
-
-        //    // Display New Movie Release
-        //    $("#newMovRel_Results").append(movieReleases);
-
-        //} else if ((i + 1) % 4 === 0) { // Set Row Closing
-        //    console.log("Forth i = " + i);
-        //    movieReleases = movieReleases.concat(
-        //        '<div class="col-sm-3">' +
-        //        '<img class="img-fluid" src="' + posterImg + '" alt=' + movieTitle + '>' + // Poster Image
-        //        //'<div class="carousel-caption fixed-top" id="movieInfo">' +
-        //        //    '<h1>' + movieTitle + '</h1>' + // Movie Title
-        //        //    '<hr />' +
-        //        //    '<h4 class="text-left">Overview:</h4>' +
-        //        //    '<p class="text-left">' + movieOverview + '</p>' + // Movie Overview Details
-        //        //    '<p class="text-left">Average Vote: ' + voteAvg + '/10 <small> -- Vote Count: ' + voteCount + '</small></p>' + // Movie Ratings
-        //        //    '<button type="button" class="btn btn-primary video-btn" id="trailerButtonID" data-toggle="modal" data-src="' + movieID +
-        //        //    '" data-target="#myModal">Watch Trailer</button >' + // Button to Watch Trailer
-        //        //'</div>' +
-        //        '</div>' +
-        //        '</div>');
-        //    // Display New Movie Release
-        //    $("#newMovRel_Results").append(movieReleases);         
-        //} else {
-
-        //    // Create Movie Poster
-        //    movieReleases = movieReleases.concat(
-        //        '<div class="col-sm-3">' +
-        //        '<img class="img-fluid" src="' + posterImg + '" alt=' + movieTitle + '>' + // Poster Image
-        //        //'<div class="carousel-caption fixed-top" id="movieInfo">' +
-        //        //    '<h1>' + movieTitle + '</h1>' + // Movie Title
-        //        //    '<hr />' +
-        //        //    '<h4 class="text-left">Overview:</h4>' +
-        //        //    '<p class="text-left">' + movieOverview + '</p>' + // Movie Overview Details
-        //        //    '<p class="text-left">Average Vote: ' + voteAvg + '/10 <small> -- Vote Count: ' + voteCount + '</small></p>' + // Movie Ratings
-        //        //    '<button type="button" class="btn btn-primary video-btn" id="trailerButtonID" data-toggle="modal" data-src="' + movieID +
-        //        //    '" data-target="#myModal">Watch Trailer</button >' + // Button to Watch Trailer
-        //        //'</div>' +
-        //        '</div>');
-
-        //    // Display New Movie Releases
-        //    $("#newMovRel_Results").append(movieReleases);
-        //}
+        if (page > 9) {
+            $("#nextBtn").attr('disabled', 'disabled');
+        } else {
+            $("#nextBtn").removeAttr('disabled');
+        }
+        
     }
 }
 
+function nextPage() {
+    page += 1;
+    
+    LoadNMReleases();
+}
+
+function prevPage() {
+    page -= 1;
+    
+    LoadNMReleases();
+}
+
+function getPage() {
+    return page;
+}
+
+// CREDIT: https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
 function getDate() {
     today = new Date();
     var dd = today.getDate();

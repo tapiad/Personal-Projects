6ff4c02fdb4fd2761ca73b4a7b97f63ce3545d06
tapiad <dtapia12@mail.wou.edu>
2019-09-05 21:03:18 -0700

    added buttons and a text box for notes to be saved/deleted

--- X_App/X_App/X_App/MainPage.xaml.cs
+++ X_App/X_App/X_App/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,31 @@ namespace X_App
 {
     public partial class MainPage : ContentPage
     {
+
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
         public MainPage()
         {
             InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
         }
 
         int count = 0;
